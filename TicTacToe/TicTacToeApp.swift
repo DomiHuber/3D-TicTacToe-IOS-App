a3b991d37e60cac7547643bc1cc7e67e1492e755
Dominic Huber <dominic.nadim@bluewin.ch>
2022-02-13 19:47:38 +0100

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Dominic Huber on 13.02.22.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
